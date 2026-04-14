4a3cee89ef4f6494558c8df45fa7bf11259ffd73
Swara Nafade <swaranafade16@gmail.com>
2026-04-14 04:00:03 -0700

    Updated FastAPI methods

--- Week2/Day2/books.py
+++ Week2/Day2/books.py
@@ -1,25 +1,34 @@
 from fastapi import FastAPI
 app=FastAPI()
 Books=[
-    {"Title":"Ttile1","Author":"Author1","category":"science"},
-    {"Title":"Ttile2","Author":"Author2","category":"science"},
-    {"Title":"Ttile3","Author":"Author3","category":"history"},
-    {"Title":"Ttile4","Author":"Author4","category":"english"},
-    {"Title":"Ttile5","Author":"Author5","category":"science"},
+    {"Title":"Title1","Author":"Author1","category":"science"},
+    {"Title":"Title2","Author":"Author2","category":"science"},
+    {"Title":"Title3","Author":"Author3","category":"history"},
+    {"Title":"Title4","Author":"Author4","category":"english"},
+    {"Title":"Title5","Author":"Author5","category":"science"},
 ]
-'''
-Simply making a  get request
+
 @app.get("/api-endpoint")
 async def first_api():
-    return{"message":"Hey This is my FirstAPI Program"}'''
+    return{"message":"Hey This is my FirstAPI Program"}
  
 #Implementing for the books
-@app.get("/mybook")      #Requesting
+@app.get("/books")      #Requesting
 async def read_all_books():
-    return {"book_title":"My Book"}
+    return Books
  
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return {'book_title':"My favourite book"}
+
+@app.get("/books/{book_title}")
+async def read_book(book_title:str):
+    for book in Books:
+        if book.get('Title').casefold()==book_title.casefold():
+            return book
+
 #Adding a function with dynamic parameter
 @app.get("/books/{dynamic_param}")
 async def read_all_books(dynamic_param):
     return{"dynamic_param":dynamic_param}
- 
